9163101282f94a71360c90933b39b8d9dbe98ecc
Dennis O'Keeffe <hello@dennisokeeffe.com>
2020-06-22 08:41:27 +1000

    feat: Update for latest

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 1. [Octokit - GitHub](https://github.com/octokit/rest.js/)
 2. [Octokit - Documentation](https://octokit.github.io/rest.js/)
+3. [Creating a Personal Access Token](https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line)
 
 ## Adding a file to a repo
 
@@ -46,11 +47,19 @@ const octokit = new Octokit({
 const content = fs.readFileSync('file/to/path', 'utf-8');
 const fileOutput = Base64.encode(content);
 
-const data = await octokit.repos.createOrUpdateFile({
-  owner: `Dennis O'Keeffe`,
-  repo: 'okeeffed/repo-name',
-  path: 'output.md',
-  message: 'Action file output',
-  content: fileOutput,
+const { data } = await octokit.repos.createOrUpdateFileContents({
+  owner: 'okeeffed',
+  repo: 'octokit-create-file-example',
+  path: 'OUTPUT.md',
+  message: 'feat: Added OUTPUT.md programatically',
+  content: contentEncoded,
+  committer: {
+    name: `Octokit Bot`,
+    email: 'hello@dennisokeeffe.com',
+  },
+  author: {
+    name: 'Octokit Bot',
+    email: 'hello@dennisokeeffe.com',
+  },
 });
 ```
--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const { Octokit } = require('@octokit/rest');
 const { Base64 } = require('js-base64');
 const fs = require('fs');
 
-if (process.env.MODE !== 'production') {
-  require('dotenv').config();
-}
+require('dotenv').config();
 
 const octokit = new Octokit({
   auth: process.env.GITHUB_ACCESS_TOKEN,
@@ -15,12 +13,20 @@ const main = async () => {
     const content = fs.readFileSync('./input.txt', 'utf-8');
     const contentEncoded = Base64.encode(content);
 
-    const data = await octokit.repos.createOrUpdateFile({
-      owner: `Dennis O'Keeffe`,
-      repo: 'okeeffed/octokit-create-file-example',
+    const { data } = await octokit.repos.createOrUpdateFileContents({
+      owner: 'okeeffed',
+      repo: 'octokit-create-file-example',
       path: 'OUTPUT.md',
       message: 'feat: Added OUTPUT.md programatically',
       content: contentEncoded,
+      committer: {
+        name: `Octokit Bot`,
+        email: 'hello@dennisokeeffe.com',
+      },
+      author: {
+        name: 'Octokit Bot',
+        email: 'hello@dennisokeeffe.com',
+      },
     });
 
     console.log(data);
